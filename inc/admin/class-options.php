6ca7eb765f13861b5b931d5189aa55a557e0477d
TruongTuyen <hieudieuluong@gmail.com>
2019-02-18 23:37:33 +0700

    Add set_tab

--- inc/admin/class-options.php
+++ inc/admin/class-options.php
@@ -59,10 +59,6 @@ class Started_Plugin_Options {
 	public function add_menu_pages() {
 		add_menu_page( esc_html__( 'Started Plugin', 'started-plugin' ), esc_html__( 'Started Plugin', 'started-plugin' ), 'manage_options', 'started-plugin', array( $this, 'page_content' ) );
 		// add_submenu_page( 'started-plugin', esc_html__( 'Settings', 'started-plugin' ), esc_html__( 'Settings', 'started-plugin' ), 'manage_options', 'started-plugin', array( $this, 'page_content' ) ); .
-		
-		echo "<pre>";
-		print_r( $this->option_configs );
-		echo "</pre>";
 	}
 
 	public function page_content() {
@@ -70,8 +66,22 @@ class Started_Plugin_Options {
 		<div class="wrap">
 			<h1><?php esc_html_e( 'Settings', 'started-plugin' ); ?></h1>
 			<form method="post" action="options.php">
-				
-				<?php cmb2_get_metabox( 'pm-metabox-id-', 'started-plugin', 'options-page' )->show_form(); ?>
+				<?php
+				foreach ( $this->option_configs as $tab_config ) {
+					$tab_url = add_query_arg( array( 'tab' => $tab_config['id'] ), menu_page_url( 'started-plugin', false ) );
+					$current_tab = $this->option_configs[0]['id'];
+					if ( isset( $_GET['tab'] ) && $tab_config['id'] == $_GET['tab'] ) {
+						$current_tab = sanitize_text_field( $_GET['tab'] );
+					}
+					?>
+					<a href="<?php echo esc_url( $tab_url ); ?>" id="<?php echo esc_attr( $tab_config['id'] ); ?>" class="tab-nav <?php if ( $current_tab == $tab_config['id'] ) {
+						echo 'active'; } ?>"><?php echo esc_html( $tab_config['title'] ); ?></a>
+					<?php
+					if ( $current_tab == $tab_config['id'] ) {
+						cmb2_get_metabox( $tab_config['id'], 'started-plugin', 'options-page' )->show_form();
+					}
+				}
+				?>
 				<?php submit_button(); ?>
 			</form>
 		</div>
@@ -80,38 +90,66 @@ class Started_Plugin_Options {
 	}
 
 	function register_metabox() {
-		echo "<pre>";
-		print_r( $this->option_configs );
-		echo "</pre>";
+		if ( is_array( $this->option_configs ) && ! empty( $this->option_configs ) ) {
+			foreach ( $this->option_configs as $tab_config ) {
+				$cmb = new_cmb2_box(
+					array(
+						'id'           => $tab_config['id'],
+						'hookup'       => false,
+						'object_types' => array( 'options-page' ),
+					)
+				);
 
-		$cmb = new_cmb2_box(
-			array(
-				'id'           => 'pm-metabox-id-',
-				'hookup'       => false,
-				'object_types' => array( 'options-page' ),
-			)
-		);
+				if ( is_array( $tab_config['fields'] ) && ! empty( $tab_config['fields'] ) ) {
+					foreach ( $tab_config['fields'] as $field ) {
+						$field['id'] = $this->option_key . '[' . $field['id'] . ']';
+						$cmb->add_field( $field );
+					}
+				}
+			}
+		}
+	}
 
-		$cmb->add_field(
-			array(
-				'name' => __( 'Test Text', 'myprefix' ),
-				'desc' => __( 'field description (optional)', 'myprefix' ),
-				'id'   => 'test_text',
-				'type' => 'text',
-			// 'default' => 'Default Text',
-			)
-		);
-
-		$cmb->add_field(
-			array(
-				'name'    => __( 'Test Color Picker', 'myprefix' ),
-				'desc'    => __( 'field description (optional)', 'myprefix' ),
-				'id'      => 'test_colorpicker',
-				'type'    => 'colorpicker',
-				'default' => '#bada55',
-			)
-		);
+	public static function set_tab( $args ) {
+		self::$_instance->option_configs[] = $args;
 	}
 }
 
-$plugin_options = new Started_Plugin_Options();
+function started_plugin_options() {
+	return Started_Plugin_Options::instance();
+}
+
+started_plugin_options();
+
+started_plugin_options()->set_tab(
+	array(
+		'title' => 'Tab 01',
+		'id'    => 'tab_1_id',
+		'fields' => array(
+			array(
+				'name'    => esc_html__( 'Site Background Color', 'cmb2' ),
+				'desc'    => esc_html__( 'field description (optional)', 'cmb2' ),
+				'id'      => 'bg_color',
+				'type'    => 'colorpicker',
+				'default' => '#ffffff',
+			),
+			array(
+				'name'    => esc_html__( 'Test Radio', 'cmb2' ),
+				'desc'    => esc_html__( 'field description (optional)', 'cmb2' ),
+				'id'      => 'radio',
+				'type'    => 'radio',
+				'options' => array(
+					'option1' => esc_html__( 'Option One', 'cmb2' ),
+					'option2' => esc_html__( 'Option Two', 'cmb2' ),
+					'option3' => esc_html__( 'Option Three', 'cmb2' ),
+				),
+			),
+			array(
+				'name' => esc_html__( 'Test Text Area for Code', 'cmb2' ),
+				'desc' => esc_html__( 'field description (optional)', 'cmb2' ),
+				'id'   => 'textarea_code',
+				'type' => 'textarea_code',
+			),
+		),
+	)
+);
--- inc/admin/option-configs.php
+++ inc/admin/option-configs.php
@@ -11,30 +11,7 @@ $option_box[] = array(
 		array(
 			'tab_title' => '',
 			'settings'  => array(
-				array(
-					'name'    => esc_html__( 'Site Background Color', 'cmb2' ),
-					'desc'    => esc_html__( 'field description (optional)', 'cmb2' ),
-					'id'      => 'bg_color',
-					'type'    => 'colorpicker',
-					'default' => '#ffffff',
-				),
-				array(
-					'name'    => esc_html__( 'Test Radio', 'cmb2' ),
-					'desc'    => esc_html__( 'field description (optional)', 'cmb2' ),
-					'id'      => 'radio',
-					'type'    => 'radio',
-					'options' => array(
-						'option1' => esc_html__( 'Option One', 'cmb2' ),
-						'option2' => esc_html__( 'Option Two', 'cmb2' ),
-						'option3' => esc_html__( 'Option Three', 'cmb2' ),
-					),
-				),
-				array(
-					'name' => esc_html__( 'Test Text Area for Code', 'cmb2' ),
-					'desc' => esc_html__( 'field description (optional)', 'cmb2' ),
-					'id'   => 'textarea_code',
-					'type' => 'textarea_code',
-				),
+				
 			),
 		),
 	),
